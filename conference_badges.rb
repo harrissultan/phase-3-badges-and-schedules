bfe651f71833b495bf5bf10ff5fd2c5d57e5e562
Harris Sultan <harris_sultan@outlook.com>
2022-02-24 20:32:04 -0500

    commit tree 57bf5412ae403c119f3c90761abca3ff5e924b1f

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map do |name|
+        badge_maker(name)
+    end
+end
+
+def assign_rooms(room_name)
+    room_name.map.with_index(1) do |name, num|
+        "Hello, #{name}! You'll be assigned to room #{num}!"
+    end
+end
+def printer(print_name)
+    batch_badge_creator(print_name).each do |names|
+        puts names
+    end
+    assign_rooms(print_name).each do |room|
+        puts room
+    end
+end
